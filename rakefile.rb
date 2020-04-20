3672448f1c2ff6b7319524ad384435c04afd98a9
Oliver Mueller <oliver.mueller@esrlabs.com>
2020-04-20 17:19:56 +0200

    Streamline CI

--- rakefile.rb
+++ rakefile.rb
@@ -1,129 +1,66 @@
+# frozen_string_literal: true
+
 require 'rake'
-EXE_NAME="enforcer"
-HOME=ENV['HOME']
-module OS
-  def OS.windows?
-    (/cygwin|mswin|mingw|bccwin|wince|emx/ =~ RUBY_PLATFORM) != nil
-  end
+require './rake_extensions.rb'
 
-  def OS.mac?
-   (/darwin/ =~ RUBY_PLATFORM) != nil
-  end
+EXE_NAME = 'enforcer'
+HOME = ENV['HOME']
 
-  def OS.unix?
-    !OS.windows?
-  end
-
-  def OS.linux?
-    OS.unix? and not OS.mac?
-  end
-
-  def OS.jruby?
-    RUBY_ENGINE == 'jruby'
-  end
-end
 task :default do
-  puts "no default task"
+  puts 'no default task'
   create_changelog
 end
-desc "run tests"
-task :test do
-  sh "cargo test -q"
-end
-desc "run tests with printing to stdout"
-task :test_nocapture do
-  sh "cargo test -q -- --nocapture"
-end
-desc "push tag to github"
-task :push do
-  sh "git push origin"
-  current_version = get_current_version
-  sh "git push origin #{current_version}"
-end
-def create_changelog(current_version, next_version)
-  current_version = get_current_version
-  raw_log = `git log --format=%B #{current_version}..HEAD`.strip
-  log_lines = raw_log.split(/\n/)
-  log = log_lines
-          .reject{|x| x.strip == ""}
-          .collect {|line| "  * #{line}"}.join("\n")
 
-  date = Time.now.strftime("%m/%d/%Y")
-  log_entry = "### [#{next_version}] - #{date}\n#{log}"
-  puts "logmessages: #{log}"
-  ['README.md'].each do |file|
-    text = File.read(file)
-    new_contents = text.gsub(/^#\sChangelog/, "# Changelog\n\n#{log_entry}")
-    File.open(file, "w") { |f| f.puts new_contents }
-  end
+desc 'run tests'
+task :test do
+  sh 'cargo test -q'
 end
+
+desc 'run tests with printing to stdout'
+task :test_nocapture do
+  sh 'cargo test -q -- --nocapture'
+end
+
 def build_the_release
-  sh "cargo build --release"
+  sh 'cargo build --release'
   current_version = get_current_version
-  release_folder = "target/release"
-  os_ext = "darwin"
+  release_folder = 'target/release'
+  os_ext = 'darwin'
   if OS.linux?
-    os_ext = "linux"
+    os_ext = 'linux'
   elsif OS.windows?
-    os_ext = "windows"
-    release_folder = "target/x86_64-pc-windows-gnu/release"
+    os_ext = 'windows'
+    release_folder = 'target/x86_64-pc-windows-gnu/release'
   end
-  cd "#{release_folder}" do
-    cp "#{EXE_NAME}","#{HOME}/bin/#{EXE_NAME}"
+  cd release_folder.to_s do
+    cp EXE_NAME.to_s, "#{HOME}/bin/#{EXE_NAME}"
     sh "tar -cvzf #{EXE_NAME}@#{current_version}-#{os_ext}.tgz #{EXE_NAME}"
   end
 end
+
 def build_the_release_windows
-  sh "cargo build --release --target=x86_64-pc-windows-gnu"
+  sh 'cargo build --release --target=x86_64-pc-windows-gnu'
   current_version = get_current_version
-  release_folder = "target/x86_64-pc-windows-gnu/release"
+  release_folder = 'target/x86_64-pc-windows-gnu/release'
   tgz_file = "#{EXE_NAME}@#{current_version}-win64.tgz"
-  cd "#{release_folder}" do
+  cd release_folder.to_s do
     sh "tar -cvzf #{tgz_file} #{EXE_NAME}.exe"
   end
-  mv "#{release_folder}/#{tgz_file}", "target/release"
-end
-def build_the_release_windows32
-  sh "cargo build --release --target=i686-pc-windows-gnu"
-  current_version = get_current_version
-  release_folder = "target/i686-pc-windows-gnu/release"
-  tgz_file = "#{EXE_NAME}@#{current_version}-win32.tgz"
-  cd "#{release_folder}" do
-    sh "tar -cvzf #{tgz_file} #{EXE_NAME}.exe"
-  end
-  mv "#{release_folder}/#{tgz_file}", "target/release"
-end
-desc "create new version and release"
-task :create_release do
-  require 'highline'
-  cli = HighLine.new
-  cli.choose do |menu|
-    default = :minor
-    menu.prompt = "this will create and tag a new version (default: #{default}) "
-    menu.choice(:minor) do
-      next_version = get_next_version(:minor)
-      puts "create minor version with version #{next_version}"
-      create_and_tag_new_version(next_version)
-      build_the_release()
-    end
-    menu.choice(:major) do
-      next_version = get_next_version(:major)
-      puts "create major version with version #{next_version}"
-      create_and_tag_new_version(next_version)
-      build_the_release()
-    end
-    menu.choice(:patch) do
-      next_version = get_next_version(:patch)
-      puts "create patch version with version #{next_version}"
-      create_and_tag_new_version(next_version)
-      build_the_release()
-    end
-    menu.choice(:abort) { cli.say("ok...maybe later") }
-    menu.default = default
-  end
+  mv "#{release_folder}/#{tgz_file}", 'target/release'
 end
 
-desc "build release, no version bump"
+def build_the_release_windows32
+  sh 'cargo build --release --target=i686-pc-windows-gnu'
+  current_version = get_current_version
+  release_folder = 'target/i686-pc-windows-gnu/release'
+  tgz_file = "#{EXE_NAME}@#{current_version}-win32.tgz"
+  cd release_folder.to_s do
+    sh "tar -cvzf #{tgz_file} #{EXE_NAME}.exe"
+  end
+  mv "#{release_folder}/#{tgz_file}", 'target/release'
+end
+
+desc 'build release, no version bump'
 task :build_release do
   build_the_release
   if OS.linux?
@@ -132,110 +69,76 @@ task :build_release do
   end
 end
 
-namespace :version do
-  desc 'bump patch level'
-  task :patch do
-    next_version = get_next_version(:patch)
-    puts "next_version=#{next_version}"
-    create_and_tag_new_version(next_version)
-  end
-  desc 'bump minor level'
-  task :minor do
-    next_version = get_next_version(:minor)
-    puts "next_version=#{next_version}"
-    create_and_tag_new_version(next_version)
-  end
-  desc 'bump major level'
-  task :major do
-    next_version = get_next_version(:major)
-    puts "next_version=#{next_version}"
-    create_and_tag_new_version(next_version)
-  end
+desc 'build release'
+task :build do
+  sh 'cargo build --release'
+  pack_release
 end
 
-def get_next_version(jump)
-  current_version = get_current_version
-  v = Version.new(current_version)
-  v.send(jump)
+desc 'create new version and release'
+task :create_release do
+  current_tag = `git describe --tags`
+  versioner = Versioner.for(:cargo_toml, '.')
+  current_version = versioner.get_current_version
+  unless current_tag.start_with?(current_version)
+    raise "current tag #{current_tag} does not match current version: #{current_version}"
+  end
+
+  do_create_release(versioner)
 end
 
-def get_current_version
-  current_version = nil
-  ['Cargo.toml'].each do |file|
-    text = File.read(file)
-    if match = text.match(/^version\s=\s\"(.*)\"/i)
-      current_version = match.captures[0]
-      puts "version was: #{current_version}"
+def do_create_release(versioner)
+  require 'highline'
+  cli = HighLine.new
+  cli.choose do |menu|
+    default = :minor
+    menu.prompt = "this will create and tag a new version (default: #{default}) "
+    menu.choice(:minor) do
+      create_and_tag_new_version(versioner, :minor)
     end
-  end
-  current_version
-end
-def update_toml(new_version)
-  ['Cargo.toml'].each do |file|
-    text = File.read(file)
-    new_contents = text.gsub(/^version\s=\s\"\d+\.\d+\.\d+\"/, "version = \"#{new_version}\"")
-    File.open(file, "w") { |f| f.puts new_contents }
+    menu.choice(:major) do
+      create_and_tag_new_version(versioner, :major)
+    end
+    menu.choice(:patch) do
+      create_and_tag_new_version(versioner, :patch)
+    end
+    menu.choice(:abort) { cli.say('ok...maybe later') }
+    menu.default = default
   end
 end
 
-def assert_tag_exists(version)
-  raise "tag #{version} missing" if `git tag -l #{version}`.length == 0
-end
-def create_and_tag_new_version(next_version)
-  sh "cargo test -q"
-  current_version = get_current_version
+def create_and_tag_new_version(versioner, jump)
+  current_version = versioner.get_current_version
+  next_version = versioner.get_next_version(jump)
   assert_tag_exists(current_version)
   create_changelog(current_version, next_version)
-  update_toml(next_version)
-  sh "cargo build"
-  sh "git add ."
-  sh "git commit -m \"[](chore): version bump from #{current_version} => #{next_version.to_s}\""
-  sh "git tag #{next_version.to_s}"
-  puts "to undo the last commit and the tag, execute:"
-  puts "git reset --hard HEAD~1 && git tag -d #{next_version.to_s}"
+  versioner.increment_version(jump)
+  sh 'git add .'
+  commit_cmd = "git commit -m \"Bump version from #{current_version} => #{next_version}\""
+  tag_cmd = "git tag #{next_version}"
+  puts "to commit, you can use ====> git add .; #{commit_cmd}"
+  puts "to tag, use ===============> \"#{tag_cmd}\""
 end
 
-class Version < Array
-  def initialize s
-    super(s.split('.').map { |e| e.to_i })
-  end
-  def as_version_code
-    get_major*1000*1000 + get_minor*1000 + get_patch
-  end
-  def < x
-    (self <=> x) < 0
-  end
-  def > x
-    (self <=> x) > 0
-  end
-  def == x
-    (self <=> x) == 0
-  end
-  def patch
-    patch = self.last
-    self[0...-1].concat([patch + 1])
-  end
-  def minor
-    self[1] = self[1] + 1
-    self[2] = 0
-    self
-  end
-  def major
-    self[0] = self[0] + 1
-    self[1] = 0
-    self[2] = 0
-    self
-  end
-  def get_major
-    self[0]
-  end
-  def get_minor
-    self[1]
-  end
-  def get_patch
-    self[2]
-  end
-  def to_s
-    self.join(".")
+def pack_release
+  require 'zip'
+  exe_file = if OS.windows?
+               "#{EXE_NAME}.exe"
+             else
+               EXE_NAME
+             end
+  exe_path = "target/release/#{exe_file}"
+
+  zipfile_name = if OS.windows?
+                   "#{EXE_NAME}_win.zip"
+                 elsif OS.mac?
+                   "#{EXE_NAME}_mac.zip"
+                 else
+                   "#{EXE_NAME}_linux.zip"
+                 end
+  rm_f zipfile_name
+
+  Zip::File.open(zipfile_name, Zip::File::CREATE) do |zipfile|
+    zipfile.add(exe_file, exe_path)
   end
 end
